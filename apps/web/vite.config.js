d93b73a2f4ae35a657a4349409f2d59856a53913
tau34 <109936561+tau34@users.noreply.github.com>
2026-09-21 10:49:01 +0900

    set allowedHosts

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-    plugins: [react()]
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "cozytadokoro.onrender.com",
+      "cozyt.onrender.com",
+      "localhost"
+    ]
+  }
 });
